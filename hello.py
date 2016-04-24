e7dbfbcba89043bca6c5b1e7375fdf7a6de40aca
Anderson <anderson.lenin.silva@gmail.com>
2016-04-24 10:45:59 +0000

    Lecture 23 - Debugging

--- hello.py
+++ hello.py
@@ -8,9 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    # n: next, c: continue
+    #import pdb; pdb.set_trace()
+    i = 3
+    i += 1
+    visited = i
+    return "You've visited {} times".format(str(visited))
     
 if __name__ == '__main__':
     host = os.getenv('IP', '0,0,0,0')
     port = int(os.getenv('PORT', 5000))
+    app.debug = True
     app.run(host=host, port=port)
